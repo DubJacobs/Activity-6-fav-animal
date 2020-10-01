e4a998e847a1227f7b34b0856497d73b47c6d101
william jacobs <williamjacobs@williams-MacBook-Pro.local>
2020-09-30 21:04:11 -0700

    created post and get routes for users

--- fwatter/routes/apiRoutes/userRoutes/index.js
+++ fwatter/routes/apiRoutes/userRoutes/index.js
@@ -1,10 +1,18 @@
 const router = require('express').Router();
+const connection = require('../../../config/connections');
 
-// api prepended to every Route
-
+// /api/users prepended to every Route
 router.route('/')
-  .get(((_req, res) => {
-    res.send('Hello');
-  }));
+  .get(async (_req, res) => {
+    const query = 'SELECT * FROM users;';
+    const [rows] = await connection.query(query);
+    res.json(rows);
+  })
+  .post(async (req, res) => {
+    const userInput = req.body;
+    const query = 'INSERT INTO users SET ?;';
+    const result = await connection.query(query, userInput);
+    res.json(result);
+  });
 
 module.exports = router;
